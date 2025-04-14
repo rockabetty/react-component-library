df0d7a1986a37cd9254abb7dc2fd6bc419f4c394
rockabetty <marissafromtexas@gmail.com>
2025-04-13 21:10:07 -0700

    Adding FieldGroup to index export

--- src/Form/index.ts
+++ src/Form/index.ts
@@ -4,3 +4,4 @@ export {default as TextArea} from './TextArea';
 export {default as Checkbox} from './Checkbox';
 export {default as Radio} from './Radio';
 export {default as DropdownSelect } from './Select';
+export {default as FieldGroup } from './FieldGroup';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { Button, ButtonSet, Badge } from './Button';
 export { Link } from './Link';
-export { TextArea, Form, TextInput, Checkbox, Radio, DropdownSelect } from './Form';
+export { TextArea, Form, TextInput, Checkbox, Radio, DropdownSelect, FieldGroup } from './Form';
 export { Modal } from './Modal';
 export { Accordion } from './Accordion';
 export { ImageUpload, useImageUploader, Gallery, Thumbnail, Image } from './Image';
