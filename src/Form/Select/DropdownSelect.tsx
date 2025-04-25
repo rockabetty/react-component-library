4229081e562bcf0e7cd647022ca144324c0f8bed
rockabetty <marissafromtexas@gmail.com>
2025-04-24 18:11:18 -0700

    Multi Select

--- src/Form/Select/DropdownSelect.tsx
+++ src/Form/Select/DropdownSelect.tsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import classNames from 'classnames';
-import '../../design/style.css';
-import '../Form.css';
-import FormField from '../atoms/FormField';
-import {InputProps} from '../../types/input';
-import {default as Label} from '../atoms/Label';
-import { WidthSize } from '../atoms/WriteInField';
+import React, { useState, useEffect } from "react";
+import classNames from "classnames";
+import "../../design/style.css";
+import "../Form.css";
+import FormField from "../atoms/FormField";
+import { InputProps } from "../../types/input";
+import { default as Label } from "../atoms/Label";
+import { WidthSize } from "../atoms/WriteInField";
 
 type Option = {
   value: string;
   labelText: string;
   disabled?: boolean;
-  onClick?: () => {};
-}
+  onClick?: () => void;
+};
 
 export interface SelectProps extends InputProps {
   options: Option[];
   multiple?: boolean;
-  size: WidthSize
+  size: WidthSize;
+  onMultiChange?: (selectedValues: string[]) => void;
 }
 
 const DropdownSelect = (props: SelectProps) => {
-
   const {
     placeholderText,
     disabled,
     id,
     name,
     onChange,
+    onMultiChange,
     tabIndex,
     value,
     labelText,
@@ -36,56 +37,81 @@ const DropdownSelect = (props: SelectProps) => {
     errorText,
     multiple,
     size,
-    status
+    status,
   } = props;
-  
-  return(
+
+  const [selectedValues, setSelectedValues] = useState<string[]>(
+    multiple && value ? (Array.isArray(value) ? value : [value]) : [],
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedOptions = Array.from(e.target.selectedOptions).map(
+      (option) => option.value,
+    );
+
+    if (multiple) {
+      setSelectedValues(selectedOptions);
+      if (onMultiChange) {
+        onMultiChange(selectedOptions);
+      }
+    } else {
+      if (onChange) {
+        onChange(e);
+      }
+    }
+  };
+
+  const computedValue = multiple ? selectedValues : value;
+
+  return (
     <FormField
       classes={classNames({
-        'Compact': true,
-        'Disabled': disabled,
-        'Error': status === 'error',
-        [`FieldWidth-${size || 'full'}`]: true
+        Compact: true,
+        Disabled: disabled,
+        Error: status === "error",
+        [`FieldWidth-${size || "full"}`]: true,
       })}
     >
-      <Label labelText={labelText}/>
+      <Label labelText={labelText} />
       <select
         className={`form-field_control ${classNames({
-          'Valid' : status === 'valid',
-          'Error' : status === 'error'
+          Valid: status === "valid",
+          Error: status === "error",
         })}`.trim()}
         disabled={disabled}
         id={id}
         name={name}
-        onChange={onChange}
+        onChange={handleChange}
         tabIndex={tabIndex}
-        value={value}
+        value={computedValue}
         multiple={multiple}
+        {...(multiple ? { size: Math.min(options.length, 5) } : {})} // Optional: show multiple options for multiselect
       >
-        {placeholderText && (
-          <option disabled value="">{placeholderText}</option>
+        {placeholderText && !multiple && (
+          <option disabled value="">
+            {placeholderText}
+          </option>
         )}
-        {options.map((option, idx) => {
-          return (
-            <option
-              key={`dropdown-select-${idx}`}
-              className="form-field_option"
-              value={option.value} 
-              label={option.labelText} 
-              disabled={option.disabled} 
-              onClick={option.onClick}
-            />)
-        })}
+        {options.map((option, idx) => (
+          <option
+            key={`dropdown-select-${idx}`}
+            className="form-field_option"
+            value={option.value}
+            label={option.labelText}
+            disabled={option.disabled}
+            onClick={option.onClick}
+          >
+            {option.labelText}
+          </option>
+        ))}
       </select>
-      { helperText || status === 'error' ?
+      {helperText || status === "error" ? (
         <p className="form-field_helpertext">
-          {status === 'error' ? errorText : helperText }
+          {status === "error" ? errorText : helperText}
         </p>
-        :
-        null
-      }
+      ) : null}
     </FormField>
-  )
-}
+  );
+};
 
-export default DropdownSelect
+export default DropdownSelect;
